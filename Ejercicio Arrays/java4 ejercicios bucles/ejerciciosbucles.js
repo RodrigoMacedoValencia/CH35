5f8d13c3337bd0958809ac72108cab5f960acd49
RodrigoMacedoValencia <rodrigomacedovalencia@gmail.com>
2023-12-17 22:30:27 -0600

    Actualizado ejercicios array

---
+++ Ejercicio Arrays/java4 ejercicios bucles/ejerciciosbucles.js
@@ -0,0 +1,62 @@
+/*
+*Ejercicio de Arrays:
+Descripción:
+1.- Supongamos que tienes una lista de temperaturas diarias en grados Celsius para una semana. Tienes que convertir estas temperaturas a grados Fahrenheit y mostrar los resultados.
+* Temperaturas diarias en grados Celsius para una semana
+const temperaturasCelsius = [22, 25, 19, 30, 27, 21, 18];
+*Instrucciones:
+
+Crea una función llamada celsiusToFahrenheit que tome una temperatura en grados Celsius y la convierta a grados Fahrenheit usando la fórmula: F = C * (9/5) + 32.
+Utiliza un bucle para recorrer el array temperaturasCelsius, llama a la función celsiusToFahrenheit para cada temperatura y almacena los resultados en un nuevo array llamado temperaturasFahrenheit.
+Muestra ambos arrays (temperaturasCelsius y temperaturasFahrenheit) en la consola.
+*/
+
+const celsius = prompt("agrega en celcius: ");
+const Fahrenheit = (celsius* 1.8) + 32;
+console.log(`${celsius} grados celcius es igual a ${Fahrenheit} grados fahrenheit.`);
+
+/*2.- Ejercicios de Bucles:
+*Descripción:
+Escribe un programa que sume los primeros 10 números naturales (1 al 10) e imprima el resultado.
+
+*Instrucciones:
+
+Utiliza un bucle (for o while) para sumar los números del 1 al 10.
+Imprime el resultado de la suma en la consola.
+Sugerencia:
+Puedes usar una variable llamada suma para acumular la suma mientras recorres los números.*/
+
+//2.-suma números del 1 al 10 
+
+let suma= 0;
+for (let i=1; i<=10;i++){suma +=i;}
+console.log (suma)
+
+/*
+*Ejercicio 3 : Números Primos
+Escribe un programa que determine si un número ingresado por el usuario es primo o no. Un número primo es aquel que solo es divisible por 1 y por sí mismo. Puedes utilizar un bucle for para verificar si el número tiene algún divisor además de 1 y él mismo.*/
+
+function esPrimo(numero) {
+    for (var i = 2; i < numero; i++) {
+      if (numero % i === 0) {
+        return false;
+      }
+    }
+    return numero !== 1;
+  }
+  console.log(esPrimo(7));
+
+/*
+
+
+/*
+Ejercicio 4 : Secuencia de Fibonacci
+Escribe un programa que genere los primeros 10 términos de la secuencia de Fibonacci. La secuencia de Fibonacci comienza con 0 y 1, y cada término subsiguiente es la suma de los dos anteriores (0, 1, 1, 2, 3, 5, 8, 13, 21, ...).
+
+*/
+var i;
+var fib = [0, 1]; 
+for (i = 2; i <= 10; i++) {
+  fib[i] = fib[i - 2] + fib[i - 1];
+  console.log(fib[i]);
+}
