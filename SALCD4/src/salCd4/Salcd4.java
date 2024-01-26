f7f5772404efc7698fde488e90615d56ac74e14f
RodrigoMacedoValencia <rodrigomacedovalencia@gmail.com>
2024-01-26 13:01:14 -0600

    lcd4 subido

---
+++ SALCD4/src/salCd4/Salcd4.java
@@ -0,0 +1,23 @@
+package salCd4;
+
+public class Salcd4 {
+
+    public void imprimir() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Salcd4 fizzBuzz = new Salcd4();
+        fizzBuzz.imprimir();
+    }
+}
